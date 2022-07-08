d1121eb7691a58b1290046a2febc015d0a9f497d
Régis Oliveira <regisoliveiramelo@gmail.com>
2022-07-08 11:19:17 -0300

    atualizado o _document para apresentar titulos

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,17 @@
-import Document from 'next/document';
+import Document, { Head, NextScript, Main, Html } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <title>spacetraveling</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
